f394348f5d2887fba616b54e9848726b139c440b
bharatm11 <bharat.mathur@hotmail.com>
2018-11-17 15:05:41 -0500

    added methods for class walker_sensor

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <ros/console.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include "ros/ros.h"
+#include "walker/walker_sensor.h"
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
