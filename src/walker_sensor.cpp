f7a9e0aa9b4af4ca233096eb7f29929b5c6bcb59
bharatm11 <bharat.mathur@hotmail.com>
2018-11-17 20:04:11 -0500

    fixed comments in walker_sensor class files

--- src/walker_sensor.cpp
+++ src/walker_sensor.cpp
@@ -33,45 +33,26 @@
 * THE POSSIBILITY OF SUCH DAMAGE.
 
 /**
-* @file walker_sensor.h
+* @file walker_sensor.cpp
 * @author Bharat Mathur
-* @date 16 November 2018
+* @date 17 November 2018
 * @copyright 2018 Bharat Mathur
-* @brief This file defines the methods for class "walker_sensor"
+* @brief This file implements the methods for class "walker_sensor"
+* This class cpp file defines data members and methods applicable for class
+* walker_sensor to read laser sensor values and determine whether the robot
+* needs to turn or not
 */
-#ifndef WALKER_SENSOR_H_
-#define WALKER_SENSOR_H_
 
-#include <geometry_msgs/Twist.h>
-#include <sensor_msgs/LaserScan.h>
-#include <ros/console.h>
-#include "ros/ros.h"
+#include "walker/walker_sensor.h"
 
-class walker_sensor {
-private:
-  /**
-  * @brief distMin is the minimum distance value recorded from the laser sensor
-  */
-  double distMin;
-public:
-  /**
-  * @brief This is the callback function for the laser sensor. It also gets the
-  * minimum distance to an obstacle
-  * @param [in] scan is a sensor_msgs::LaserScan message which contains distance
-  * data.
-  * @return void
-  */
-  void laserCallback(const sensor_msgs::LaserScanConstPtr& scan);
-  /**
-  * @brief This function determines if the robot needs to turn or not by
-  * comparing the distMin with distThreshold
-  * @param [in] distThreshold is the minimum threshold distance at which the
-  * robot is supposed to turn
-  * @return bool
-  */
-  bool detectObstacle(double distThreshold);
-};
+void walker_sensor::laserCallback(const sensor_msgs::LaserScanConstPtr& scan) {
+  this->distMin = *std::min_element(scan->ranges.begin(), scan->ranges.end());
+}
 
-
-
-#endif /* WALKER_SENSOR_H_ */
+bool walker_sensor::detectObstacle(double distThreshold) {
+  if(this->distMin < distThreshold) {
+    return true;
+  } else {
+    return false;
+  }
+}
